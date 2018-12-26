5fe78602e43276c9aa6c2e7e1dad1493a51891ef
student <student@imac-26.local>
2018-12-26 10:02:12 +0800

    Initial Commit

---
+++ ARt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ARt
+//
+//  Created by student on 2018/12/26.
+//  Copyright © 2018年 pxy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
