9fb1255423608178b5a6de5534b98d235e81c7ae
Ru Zhao <clown@rudeimac.local>
2019-03-04 18:49:42 +0800

    Create a pro named AREyes

---
+++ ARt/AREyes/AREyes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AREyes
+//
+//  Created by Ru Zhao on 2019/3/4.
+//  Copyright © 2019年 xiaoshuo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
